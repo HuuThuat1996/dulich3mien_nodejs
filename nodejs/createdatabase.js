e31ead98dee9d50a32aa959789d89c9321fb1a90
HuuThuat1996 <thuatn33@gmail.com>
2017-09-18 22:38:02 +0700

    add new file (#2)
    add folder Bucket and 2 file js

---
+++ nodejs/createdatabase.js
@@ -0,0 +1,59 @@
+var AWS = require("aws-sdk");
+AWS.config.loadFromPath('./Bucket/pathToJsonFile.json');
+AWS.config.update({
+    endpoint: "http://localhost:8000"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+
+var FestivalTable = {
+    TableName: "Festival",
+    KeySchema: [
+        { AttributeName: "FestivalID", KeyType: "HASH" },
+        { AttributeName: "FestivalName", KeyType: "RANGE" },  //Partition key
+
+    ],
+    AttributeDefinitions: [
+        { AttributeName: 'FestivalID', AttributeType: 'S' },
+        { AttributeName: "FestivalName", AttributeType: "S" },
+      
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
+
+var AreaTable = {
+    TableName: "Area",
+    KeySchema: [
+        { AttributeName: "AreaID", KeyType: "HASH" },  //Partition key
+        { AttributeName: "AreaName", KeyType: "RANGE" },  //Partition key
+    ],
+    AttributeDefinitions: [
+        { AttributeName: 'AreaID', AttributeType: 'S' },  //Partition key
+        { AttributeName: "AreaName", AttributeType: "S" },
+        
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
+
+dynamodb.createTable(FestivalTable, function (err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
+
+dynamodb.createTable(AreaTable, function (err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
