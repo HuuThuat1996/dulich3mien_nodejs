2b7557b3edd9e6b45bc4babaf7b565cc4fd33f63
nnthuan <nnthuanit1@gmail.com>
2017-09-24 06:36:33 +0700

    Thêm code thêm festival

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+
 var festivalRepository = require('./festivalRepository');
 
-var getItems = require("./Bucket/GetListItemInBucket");
+var uploadFile = require('./uploadFile');
+
 //common
 /*mimeType
 **************************************/
@@ -32,43 +34,11 @@ http.createServer(function (request, response) {
 
     //analysis request
     const URL = url.parse(request.url);
-console.log("url " +request.url);
-    console.log("method: " + request.method);
+
+    console.log("request.url " + request.url);
+    console.log("request.method: " + request.method);
 
     if (request.method == "GET") {
-        var lastIndexOfDot = -1;
-        try {
-            lastIndexOfDot = URL.pathname.lastIndexOf('.');
-        }
-        catch (exception) {
-            lastIndexOfDot = -1;
-        }
-        //compare file type for read file and return context-type in response writeHead
-        if (lastIndexOfDot > 0) {
-            try {
-                var typeFile = URL.pathname.substr(lastIndexOfDot);
-                if (typeFile != null) {
-                    for (var x in mimeType) {
-                        if ("." + x == typeFile) {
-                            fs.readFile(path.public + URL.pathname, function (err, data) {
-                                if (err) {
-                                    response.writeHead(404, "Not found");
-                                    response.end();
-                                }
-                                else {
-                                    response.writeHead(200, { 'content-type': mimeType[x] });
-                                    response.end(data);
-                                }
-                            });
-                            break;
-                        }
-                    }
-                }
-            }
-            catch (exception) {
-                response.end();
-            }
-        }
         switch (URL.pathname.toLowerCase()) {
             case "/":
                 fs.readFile(path.html + "/index.html", function (err, data) {
@@ -77,19 +47,19 @@ console.log("url " +request.url);
                         response.end();
                     }
                     else {
-                        response.writeHead(200, { 'content-type': mimeType.html });
+                        response.writeHead(200, {'content-type': mimeType.html});
                         response.end(data);
                     }
                 });
                 break;
-            case "/view":
-                fs.readFile(path.html + "/view.html", function (err, data) {
+            case "/test":
+                fs.readFile(path.html + "/test.html", function (err, data) {
                     if (err) {
                         response.writeHead(404, "Not found");
                         response.end();
                     }
                     else {
-                        response.writeHead(200, { 'content-type': mimeType.html });
+                        response.writeHead(200, {'content-type': mimeType.html});
                         response.end(data);
                     }
                 });
@@ -101,7 +71,7 @@ console.log("url " +request.url);
                         response.end();
                     }
                     else {
-                        response.writeHead(200, { 'content-type': mimeType.html });
+                        response.writeHead(200, {'content-type': mimeType.html});
                         response.end(data);
                     }
                 });
@@ -113,26 +83,25 @@ console.log("url " +request.url);
                         response.end();
                     }
                     else {
-                        response.writeHead(200, { 'content-type': mimeType.html });
+                        response.writeHead(200, {'content-type': mimeType.html});
                         response.end(data);
                     }
                 });
                 break;
-            case "/themdiadanh":
-                {
-                    console.log("có nè");
-                    fs.readFile(path.html + "/ThemDiaDanh.html", function (err, data) {
-                        if (err) {
-                            response.writeHead(404, "Not found");
-                            response.end();
-                        }
-                        else {
-                            response.writeHead(200, { 'content-type': mimeType.html });
-                            response.end(data);
-                        }
-                    });
-                    break;
-                }
+            case "/themdiadanh": {
+                console.log("có nè");
+                fs.readFile(path.html + "/ThemDiaDanh.html", function (err, data) {
+                    if (err) {
+                        response.writeHead(404, "Not found");
+                        response.end();
+                    }
+                    else {
+                        response.writeHead(200, {'content-type': mimeType.html});
+                        response.end(data);
+                    }
+                });
+                break;
+            }
             case "/xoadiadanh":
                 fs.readFile(path.html + "/XoaDiaDanh.html", function (err, data) {
                     if (err) {
@@ -140,7 +109,7 @@ console.log("url " +request.url);
                         response.end();
                     }
                     else {
-                        response.writeHead(200, { 'content-type': mimeType.html });
+                        response.writeHead(200, {'content-type': mimeType.html});
                         response.end(data);
                     }
                 });
@@ -166,8 +135,8 @@ console.log("url " +request.url);
                                             response.end();
                                         }
                                         else {
-                                            response.writeHead(200, { 'content-type': mimeType[x] });
-                                            response.writeHead(200, { 'content-type': mimeType[x] });
+                                            response.writeHead(200, {'content-type': mimeType[x]});
+                                            response.writeHead(200, {'content-type': mimeType[x]});
                                             response.end(data);
                                         }
                                     });
@@ -187,69 +156,46 @@ console.log("url " +request.url);
             }
         }
     }
-    else if (request.method == "POST") {
+    else if (request.method.toString() == "POST") {
 
-        request.on('data', function (chunk) {
-            var urlFull = request.url + "?" + chunk;
-            var model = url.parse(urlFull, true).query;
-
-            switch (URL.pathname.toLowerCase()) {
-                case "/themlehoi": {
-                    try {
-                        console.log("FestivalName: " + model.FestivalName + "\n");
-                        console.log("StartDate: " + model.StartDate + "\n");
-                        console.log("EndDate: " + model.EndDate + "\n");
-                        console.log("Address: " + model.Address + "\n");
-                        console.log("Descript: " + model.Descript + "\n");
-                        console.log("ImageTile: " + model.ImageTitle + "\n");
-                        console.log("Image: " + model.Image + "\n");
-                        //1, Thêm hình đại diện vào Bucket hình ảnh
-                        // createBucket(model.FestivalName, model.FestivalName);
-                        // put hình đại diện
-                        // putItems(model.FestivalName, "HinhDaiDien", "đường dẫn file");
-                        // put danh sách các file trong image
-                        // putItems(model.FestivalName, "ten cua file . phần mở rộng", "đường dẫn của file");
-                         getItems.GetList("DuLich3Mien",function(_lst){
-                            var a = _lst.split('%');
-                            for (i = 0; i < a.lenght; i++) {
-                                console.log("link " + a[i]);
-                            }
-                        });
-                        
-                        //2, Lấy thông tin hình đại diện trên s3 với cấu trúc
-                        /*{
-                            public_id: '',
-                            version: ,
-                            signature: '',
-                            width: ,
-                            height: ,
-                            format: '',
-                            resource_type: '',
-                            created_at: '2017-06-46:03Z',
-                            bytes: ,
-                            type: '',
-                            url: '',
-                            secure_url: '',
-                            name:''
-                        }*/
-                        //3, Tương tự như trên nhưng làm với nhiều hình ảnh
-
-                        //4, Thêm thông tin festival vào dynamodb sử dụng hàm festivalRepository.addFestival()
-                        //5, Trả kết quả thực hiện lại cho client
-                        break;
-                    }
-                    catch (exception) {
-                        console.error("Exception. exception JSON:", JSON.stringify(err, null, 2));
-                        response.writeHead();
-                        response.end();
-                    }
+        switch (URL.pathname.toLowerCase()) {
+            case "/themlehoi": {
+                try {
+                    uploadFile.uploadFiles(request, function (err, fields, files) {
+                        if (err) {
+                            response.writeHead(500, {'content-type': 'text/plain'});
+                            response.end("Fail");
+                        }
+                        else {
+                            festivalRepository.addFestival(fields.FestivalName, fields.StartDate, fields.EndDate, fields.Address, fields.Descript, files.ImageTitle, files.Image, function (result) {
+                                if (!result) {
+                                    response.writeHead(500, {'content-type': 'text/plain'});
+                                    response.end("Fail");
+                                }
+                                else {
+                                    response.writeHead(200, {'content-type': 'text/plain'});
+                                    response.end("Success");
+                                }
+                            });
+                        }
+                    });
                 }
-
-                default:
-                    break;
+                catch (exception) {
+                    console.log("->Exception");
+                    response.writeHead();
+                    response.end();
+                }
+                break;
             }
-        });
+            default:
+                break;
+        }
     }
 
 }).listen(8084);
 console.log("Server running at http://loaclhost:8084");
+// request.on('data', function (chunk) {
+//      var urlFull = request.url + "?" + chunk;
+//     var model = url.parse(urlFull, true).query;
+//     console.log(model.name);
+//     });
